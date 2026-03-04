d50ace94ea39a32c32f1a0c8f91e9df8ba24dfbc
dhruvgoel8649 <dhruvgoel8649@gmail.com>
2026-03-04 20:55:33 +0530

    DevOps pipeline setup

---
+++ terraform/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "app_server" {
+  ami           = ""
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.app_sg.name]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = file("install_docker.sh")
+
+  tags = {
+    Name = "DevOps-App-Server"
+  }
+}
